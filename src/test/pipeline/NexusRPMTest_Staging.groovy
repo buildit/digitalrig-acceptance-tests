ecc31f2964fc7b19a76f7ed7686e8fb054a3208d
rsafronov <electroma@gmail.com>
2016-12-05 21:08:54 -0500

    Fixed: do not include all files into test rpm

--- src/test/pipeline/NexusRPMTest_Staging.groovy
+++ src/test/pipeline/NexusRPMTest_Staging.groovy
@@ -2,7 +2,7 @@ node {
     sh "rm -f *.rpm"
     env.TIMESTAMP = new Date().time
     writeFile(file: 'sample', text: "this is a test file")
-    sh "/usr/local/bin/fpm -s dir -t rpm -n nexus-test -v ${TIMESTAMP} -a all ."
+    sh "/usr/local/bin/fpm -s dir -t rpm -n nexus-test -v ${TIMESTAMP} -a all sample"
     catchError {
         // upload
         sh "curl -v -u admin:admin123 --upload-file nexus-test-${TIMESTAMP}-1.noarch.rpm ${STAGING_NEXUS}"
